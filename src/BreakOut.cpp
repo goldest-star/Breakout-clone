b97526926ceab6619cf5a1a650fa1c5c5fba0e1c
Breakout-clone <cristiano900520@gmail.com>
2021-02-09 11:37:04 -0500

    cleanup
    Removed commented code and fixed whitespace

--- src/BreakOut.cpp
+++ src/BreakOut.cpp
@@ -8,37 +8,27 @@ BreakOut::BreakOut() { sAppName = "BreakOut"; }
 bool
 BreakOut::OnUserCreate()
 {
-	
 	blocks_ = std::make_unique<World>();
 	bat = std::make_unique<Bat>(*this, *blocks_);
 	ball = std::make_unique<Ball>(*this, *blocks_, *bat );
-//	fragment = std::make_unique<Fragment>(*this, *blocks_);
-//	ball->reset(blocks_->width() / 2.0f, blocks_->height() / 2.0f);
-
 	return true;
 }
 
 bool
 BreakOut::OnUserUpdate(float fElapsedTime)
 {
-	// Quit key
+	// Quit game
 	if (GetKey(olc::Key::Q).bPressed)
 		exit(0);
 
-	if (GetKey(olc::Key::SPACE).bPressed) {
+	// start game
+	if (GetKey(olc::Key::SPACE).bPressed)
 		playing = true;
-	}
 		
-
 	if (playing) {
 		bat->update(fElapsedTime);
 		ball->update(fElapsedTime);
-		}
-//	} else {
-//		Clear(olc::Pixel(31, 144, 255)); // blue
-//		auto msg = std::string("PRESS SPACE BAR");
-//		DrawString(olc::vi2d((blocks_->width() - msg.size() / 2) / 2, blocks_->height() / 2) * blocks_->blockSize(), msg, olc::WHITE);
-//	}
+	}
 	renderGraphics();
 	return true;
 }
@@ -55,6 +45,5 @@ BreakOut::renderGraphics()
 	blocks_->drawMap(this);
 	bat->draw();
 	ball->draw();
-//	ball->draw();
 	SetPixelMode(olc::Pixel::NORMAL); // Draw all pixels
 }

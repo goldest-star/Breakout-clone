adc52371b531bf95c3d5fae3f5c0e7a04670ba56
Breakout-clone <cristiano900520@gmail.com>
2021-03-02 11:37:18 -0500

    Rewrote collision detection and refactor
    The collision detection for the tiles and bat use the same algorithms
    now, i.e. Circle vs rectangle. There are still some bugs that need to be
    squashed, but overall its a good starting point. I also did some
    refactoring and cleaned up some the code.

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -2,6 +2,10 @@
 #include "Bat.h"
 #include "BreakOut.h"
 
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
 Ball::Ball(olc::PixelGameEngine &game, World &blocks, Bat &bat)
 		  : game_(game), blocks_(blocks), bat_(bat)
 {
@@ -17,31 +21,39 @@ Ball::update(float fElapsedTime)
 		reset(blocks_.width() / 2.0f, blocks_.height()/ 2.0f);
 
 	auto potentialPos = position_ + velocity_ * fElapsedTime;
-	hasHitTile = false;
-	hasHitTile |= testResolveCollision(potentialPos, olc::vf2d(0,-1)); // northern
-	hasHitTile |= testResolveCollision(potentialPos, olc::vf2d(0,+1)); // southern
-	hasHitTile |= testResolveCollision(potentialPos, olc::vf2d(-1,0)); // western
-	hasHitTile |= testResolveCollision(potentialPos, olc::vf2d(+1,0)); // eastern
 
-	// bat collision (circle vs rectangle collision)
-	if (potentialPos.y >= bat_.position().y) { // TODO: this check alone is not sufficient
-		if (circleVsRect(potentialPos, bat_.position(), { bat_.width(), bat_.height() })) {
-			auto incedentRay{velocity_.norm()};
-			olc::vf2d surface{ bat_.width(), 0.0f };
-			auto surfaceNormal{surface.perp().norm()};
-			// reflection ray equation: r = d - 2(d * n)n
-			auto reflectedRay{incedentRay - 2 * incedentRay.dot(surfaceNormal) * surfaceNormal};
-			velocity_ = reflectedRay * velocity_.mag();
+	// area of detection
+	olc::vi2d currentCell = position_.floor();
+	olc::vi2d targetCell = potentialPos; // why no ceiling
+	olc::vi2d worldSize{ int(blocks_.width()), int(blocks_.height()) };
+	olc::vi2d areaTopleft = (currentCell.min(targetCell) - olc::vi2d(1, 1)).max({ 0, 0 });
+	olc::vi2d areBottomRight = (currentCell.max(targetCell) + olc::vi2d(1, 1)).min(worldSize - olc::vi2d(1, 1));
+	// iterate through the cells in the limited area
+	olc::vi2d cell;
+	olc::vi2d cellSize{ 1, 1 };
+	for (cell.y = areaTopleft.y; cell.y <= areBottomRight.y; ++cell.y)
+		for (cell.x = areaTopleft.x; cell.x <= areBottomRight.x; ++cell.x) {
+			auto &tile{blocks_[cell.y * blocks_.width() + cell.x]};
+			olc::vf2d contactPnt;
+			if (tile > 0 && circleVsRect(potentialPos, cell, cellSize, contactPnt)) {
+				olc::vf2d normal{contactNormal(contactPnt, cell, cellSize)};
+				std::cout << -(position_ - contactPnt).norm() << '\n';
+				// reflection ray equation: r = d - 2n(d * n)
+				auto incedentRay{velocity_.norm()};
+				auto reflectedRay{incedentRay - 2 * normal * incedentRay.dot(normal)};
+				velocity_ = reflectedRay * velocity_.mag();
+				if (tile < 10) --tile;
+			}
 		}
-	}
+	position_ += velocity_ * fElapsedTime;
 
-	// ball position is out of bound  *** this is buggy ***
-	isOutOfBounds = position_.y > blocks_.height();
-	if (isOutOfBounds && game_.GetKey(olc::Key::SPACE).bPressed) {
+	// should use state pattern instead
+	if (position_.y > blocks_.height())
+		isOutOfBounds = true;
+	if (isOutOfBounds)
 		reset(blocks_.width() / 2.0f, blocks_.height() / 2.0f);
+	if (isOutOfBounds && game_.GetKey(olc::Key::SPACE).bPressed)
 		isOutOfBounds = false;
-	}
-	position_ += velocity_ * fElapsedTime; // delta x = delta v * delta t
 }
 
 void
@@ -74,35 +86,37 @@ Ball::reset(float x, float y)
 }
 
 bool
-Ball::testResolveCollision(const olc::vf2d &position, const olc::vf2d &point)
+Ball::circleVsRect(const olc::vf2d &pos, const olc::vf2d &rectPos, const olc::vf2d &rSize,
+		           olc::vf2d &contactPnt)
 {
-	olc::vf2d tileRadialDimensions_{ radius_ / blocks_.blockSize().x, radius_ / blocks_.blockSize().y };
-	// offset to center point of reference
-	olc::vi2d vTestPoint = position + tileRadialDimensions_ * point;
-	auto index = vTestPoint.y * blocks_.width() + vTestPoint.x;
-	auto &tile = blocks_[index];
-	if (tile == 0 || index > blocks_.size())
-		return false;
-	bool bTileHit = tile < 10;
-	if (bTileHit) {
-		hitID = tile;
-		hitpos = { float(vTestPoint.x), float(vTestPoint.y) }; 
-		--tile;
+	olc::vf2d nearestPnt;
+	nearestPnt.x = std::max(rectPos.x, std::min(rectPos.x + rSize.x, pos.x));
+	nearestPnt.y = std::max(rectPos.y, std::min(rectPos.y + rSize.y, pos.y));
+	olc::vf2d nearestRay{nearestPnt - pos};
+	float overlap{(radius_ / float(blocks_.blockSize().x)) - nearestRay.mag()};
+	if (std::isnan(overlap)) overlap = 0;
+	if (overlap > 0) { // collision
+		auto dir{nearestRay.norm()};
+		contactPnt = (pos - dir * overlap) + dir * (radius_ / 16);; 
+		return true;
 	}
-	// collision response
-	if (point.x == 0.0f) velocity_.y *= -1.0f;
-	if (point.y == 0.0f) velocity_.x *= -1.0f;
-	return true;
+	return false;
 }
 
-bool
-Ball::circleVsRect(const olc::vf2d &pos, const olc::vf2d &rectPos, const olc::vf2d &recSize)
+olc::vf2d
+Ball::contactNormal(const olc::vf2d &point, const olc::vf2d &rect, const olc::vf2d &rSize)
 {
-	auto [width, height]{recSize};
-	// clamp ball position to bat's size
-	float nx{std::max(rectPos.x, std::min(rectPos.x + width, pos.x))};
-	float ny{std::max(rectPos.y, std::min(rectPos.y + height, pos.y))};
-	olc::vf2d nearestPnt{ nx, ny };
-	olc::vf2d nearestRay{pos - nearestPnt};
-	return nearestRay.mag() <= radius_ / blocks_.blockSize().x;
+	auto [height, width]{rSize};
+	olc::vf2d rectCentre{ rect.x + height / 2.0f, rect.y + width / 2.0f };
+	auto ballPosition{ point - rectCentre };
+	auto [a, b]{ballPosition};
+	if (std::abs(a) < width / 2 && b == -height / 2) // top edge
+		return { 0.0f, -1.0f };
+	if (std::abs(a) < width / 2 && b == height / 2) // bottom edge
+		return { 0.0f, 1.0f };
+	if (a == -width / 2 && std::abs(b) < height / 2) // left edge
+		return { -1.0f, 0.0f };
+	if (a == width / 2 && std::abs(b) < height / 2) // right edge
+		return { 1.0f, 0.0f };
+	return ballPosition.norm(); // corners: left-top/right and bottom-left/right
 }
